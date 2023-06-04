d20c1c95c37c45d5fd58c41ad1597fef7f4e7089
dilip-bijoriya <dilipbijoriya6588@gmail.com>
2023-06-04 16:44:00 +0530

    cors add

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ import express from 'express';
 import { default as mongoose } from 'mongoose';
 import adminRouter from './routes/admin.route';
 import publicRouter from './routes/public.route';
+import cors from 'cors';
 const port = process.env.PORT || 4000;
 
 async function server() {
     try {
         const app = express();
         app.use(express.json());
+        app.use(cors());
         await mongoose.connect(process.env.MONGODB as string);
         console.log('MongoDB is connected.');
         app.use('/api/admin', adminRouter);
